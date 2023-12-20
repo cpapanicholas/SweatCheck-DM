89b52e75d63163f40aee40be14ad693f3508eef0
cpapanicholas <cpapanicholas@gmail.com>
2023-12-19 19:53:49 -0600

    added models

---
+++ server/models/User.js
@@ -0,0 +1,47 @@
+// server/models/User.js
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Hash the password before saving it to the database
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  if (user.isModified('password') || user.isNew) {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(user.password, salt);
+      user.password = hashedPassword;
+      next();
+    } catch (error) {
+      return next(error);
+    }
+  } else {
+    return next();
+  }
+});
+
+// Compare provided password with the hashed password in the database
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
